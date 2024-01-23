972ede1edb81e66d4fb91715a7a5fc6ab2e2c863
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-22 22:54:00 -0300

    creating function to clear irrelevant characteres

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pymongo
+import re
+from nltk.corpus
 
 client = pymongo.MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
 db = client["redditscraper"]
 posts_collection = db["posts"]
 
 posts_data = [post["title"] for post in posts_collection.find()]
+
+def clean_text(text):
+    text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
+    return ' '.join([word for word in text.split() if word not in stopwords.words('english')])
