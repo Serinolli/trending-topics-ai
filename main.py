7a36234f17dd6607f75784016ff692d4f3815df2
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-24 22:52:29 -0300

    tokenizing words

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pymongo
+import nltk
 import re
+
+nltk.download('corpus')
+nltk.download('tokenize')
+
 from nltk.corpus import stopwords
-from sklearn.model_selection import train_test_split
+from nltk.tokenize import word_tokenize
 
 client = pymongo.MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
 db = client["redditscraper"]
 posts_collection = db["posts"]
 
 posts_data = [post["title"] for post in posts_collection.find()]
+tokenized_posts = [word_tokenize(clean_text(post['title'])) for post in posts_data]
 
 def clean_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
