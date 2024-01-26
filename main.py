b6bf094b3cc699169671f9605d6b8c13228b5bdc
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-25 22:50:34 -0300

    import and using TF-IDF

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import re
 import pymongo
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def clean_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
@@ -16,4 +18,6 @@ posts_data = posts_collection.find()
 post_titles = [post["title"] for post in posts_data]
 
 tokenized_posts = [word_tokenize(clean_text(title)) for title in post_titles]
-print(tokenized_posts)
+vectorizer = TfidfVectorizer(max_features=5000)  
+X = vectorizer.fit_transform([' '.join(post) for post in tokenized_posts]).toarray()
+print(X)
