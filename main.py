d0e69d11460a325357ddfefd2fa55f5b8fed66d3
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-25 22:42:42 -0300

    correcting tokenized words

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import pymongo
-import nltk
 import re
-
-nltk.download('corpus')
-nltk.download('tokenize')
-
-from nltk.corpus import stopwords
+import pymongo
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+def clean_text(text):
+    text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
+    return ' '.join([word for word in text.split() if word not in stopwords.words('english')])
 
 client = pymongo.MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
 db = client["redditscraper"]
 posts_collection = db["posts"]
 
-posts_data = [post["title"] for post in posts_collection.find()]
-tokenized_posts = [word_tokenize(clean_text(post['title'])) for post in posts_data]
+posts_data = posts_collection.find()
 
-def clean_text(text):
-    text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
-    return ' '.join([word for word in text.split() if word not in stopwords.words('english')])
+post_titles = [post["title"] for post in posts_data]
+
+tokenized_posts = [word_tokenize(clean_text(title)) for title in post_titles]
+print(tokenized_posts)
