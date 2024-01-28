b7b7c7b7be010ad80db2ca305d1924de4eb11920
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-28 14:27:32 -0300

    starting implementations to validate content

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from validator import GetClusterMainTopics
 
+# Used functions
 def clean_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
-    return ' '.join([word for word in text.split() if word not in stopwords.words('english')])
+    stop_words = set(stopwords.words('english'))
+    return ' '.join(word for word in text.split() if word not in stop_words)    
 
 client = pymongo.MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
 db = client["redditscraper"]
 posts_collection = db["posts"]
 
 posts_data = posts_collection.find()
-
 post_titles = [post["title"] for post in posts_data]
 
 tokenized_posts = [word_tokenize(clean_text(title)) for title in post_titles]
+
 vectorizer = TfidfVectorizer(max_features=5000)  
 X = vectorizer.fit_transform([' '.join(post) for post in tokenized_posts]).toarray()
 
-kmeans = KMeans(n_clusters=5, random_state=42)
+n_clusters = 10
+kmeans = KMeans(n_clusters=n_clusters, random_state=42)
 clusters = kmeans.fit_predict(X)
+
+#starting implementations to validate content
+main_topics = []
+for clusterId in range(n_clusters):
+    main_topics.append(GetClusterMainTopics(clusterId, post_titles, clusters[clusterId], tokenized_posts))
