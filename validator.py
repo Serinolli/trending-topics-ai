6cd27e9096a963d957f73221e5352498eb1bf823
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-28 14:27:01 -0300

    creating file to valdiate the clusters

---
+++ validator.py
@@ -0,0 +1,12 @@
+def GetClusterMainTopics(cluster_id, post_titles, cluster, tokenized_posts, n_top_topics=5):
+    cluster_tf = {}
+    for i, title in enumerate(post_titles):
+        if cluster == cluster_id:
+            for term in tokenized_posts[i]:
+                cluster_tf[term] = cluster_tf.get(term, 0) + 1
+
+    total_terms = sum(cluster_tf.values())
+    cluster_term_weights = {term: freq / total_terms for term, freq in cluster_tf.items()}
+
+    top_terms = sorted(cluster_term_weights, key=cluster_term_weights.get, reverse=True)[:n_top_topics]
+    return top_terms
