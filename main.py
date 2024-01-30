72f7487bcadc48b6a90e8f874b20f51c2b9d4919
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-29 22:17:59 -0300

    moving functions to other file

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-import re
 import pymongo
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
-from validator import GetClusterMainTopics
-
-def clean_text(text):
-    text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
-    stop_words = set(stopwords.words('english'))
-    return ' '.join(word for word in text.split() if word not in stop_words)    
+from validator import clean_text, GetClusterMainTopics
 
 client = pymongo.MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
 db = client["redditscraper"]
-postsCollection = db["posts"]
+posts_collection = db["posts"]
 
-postsData = postsCollection.find()
-post_titles = [post["title"] for post in postsData]
+posts_data = posts_collection.find()
+post_titles = [post["title"] for post in posts_data]
 
-tokenizedPosts = [word_tokenize(clean_text(title)) for title in post_titles]
+tokenized_posts = [word_tokenize(clean_text(title)) for title in post_titles]
 
 vectorizer = TfidfVectorizer(max_features=5000)  
-X = vectorizer.fit_transform([' '.join(post) for post in tokenizedPosts]).toarray()
+X = vectorizer.fit_transform([' '.join(post) for post in tokenized_posts]).toarray()
 
 n_clusters = 9
 kmeans = KMeans(n_clusters=n_clusters, random_state=42)
 clusters = kmeans.fit_predict(X)
 
+cluster_record_counts = {i: 0 for i in range(n_clusters)}
+for cluster_label in clusters:
+    cluster_record_counts[cluster_label] += 1
+
+sorted_clusters = sorted(cluster_record_counts.items(), key=lambda x: x[1], reverse=True)
+
 main_topics = []
-for clusterId in range(n_clusters):
-    main_topics.append(GetClusterMainTopics(clusterId, post_titles, clusters, tokenizedPosts))
+for cluster_id, _ in sorted_clusters:
+    main_topics.append(GetClusterMainTopics(cluster_id, post_titles, clusters, tokenized_posts))
 
 for i, topics in enumerate(main_topics):
     print(f"Main topics of Cluster {i}:")
