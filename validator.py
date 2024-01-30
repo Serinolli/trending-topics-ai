c49f5eed061101d09604ff0a0625eea5ec986304
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-29 22:18:12 -0300

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,12 +1,20 @@
-def GetClusterMainTopics(clusterId, postTitles, clusters, tokenizedPosts, nTopTopics=5):
+import re
+from nltk.corpus import stopwords
+
+def clean_text(text):
+    text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
+    stop_words = set(stopwords.words('english'))
+    return ' '.join(word for word in text.split() if word not in stop_words)
+
+def GetClusterMainTopics(cluster_id, post_titles, clusters, tokenized_posts, n_top_topics=5):
     cluster_tf = {}
-    for i, title in enumerate(postTitles):
-        if clusters[i] == clusterId:
-            for term in tokenizedPosts[i]:
+    for i, title in enumerate(post_titles):
+        if clusters[i] == cluster_id:
+            for term in tokenized_posts[i]:
                 cluster_tf[term] = cluster_tf.get(term, 0) + 1
 
     total_terms = sum(cluster_tf.values())
     cluster_term_weights = {term: freq / total_terms for term, freq in cluster_tf.items()}
 
-    top_terms = sorted(cluster_term_weights, key=cluster_term_weights.get, reverse=True)[:nTopTopics]
+    top_terms = sorted(cluster_term_weights, key=cluster_term_weights.get, reverse=True)[:n_top_topics]
     return top_terms
