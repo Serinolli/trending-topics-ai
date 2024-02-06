9ab9cbb9e1180506cf02e6b52f6a8e17a2e457eb
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-06 16:35:49 -0300

    avoiding repeating content

--- output.py
+++ output.py
@@ -1,7 +1,18 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-
 def outputClustersInfo(clusters):
+    global_unique_topics = set()
     for cluster in clusters:
-        print(cluster)
+        cluster_unique_topics = set()
+        for topic in cluster:
+            if isinstance(topic, tuple):
+                cluster_unique_topics.add(' '.join(topic))
+            else:
+                cluster_unique_topics.add(topic)
+        unique_topics_in_cluster = cluster_unique_topics - global_unique_topics
+        if unique_topics_in_cluster:
+            print(unique_topics_in_cluster)
+            global_unique_topics.update(cluster_unique_topics)
+        else:
+            print("No unique topics found for this cluster.")
